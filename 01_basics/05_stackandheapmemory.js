3db6dd3c32d2366de4ae9e02e046317f187572d3
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-09 13:06:54 +0000

    stackandheapmemory.js

---
+++ 01_basics/05_stackandheapmemory.js
@@ -0,0 +1,16 @@
+// Stack
+let myytname = "ShivamCode360"
+let anothername = myytname
+anothername = "AbhayCode360"
+console.log(myytname)
+console.log(anothername)
+
+// Heap
+let user1 = {
+    email: "stk686@gmail.com",
+    upi: "dt@ybl"
+}
+let user2 = user1
+user2.email = "shivam87998@gmail.com"
+console.log(user1.email)
+console.log(user2.email)
