758278432a2a9f718d6123d550d4eaf272c9266d
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-09 05:23:01 +0000

    dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "Shivam";
+let age = 87;
+let isLoggedIn = false;
+ 
+console.log(typeof "Shivam");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof 87);
+console.log(typeof false);
