dfff9c2197634255bc439561d03ac744e888ac1e
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-09 12:38:56 +0000

    conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "78686"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Operations
+console.log(1 + 2 + "2")
+console.log(1 + "2")
+console.log(true)
+console.log(+true)
+console.log(+"")
