9e9a541dfb48ff79a39da061fbe17ebe5910d2fe
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-09 05:12:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accId = 57556;
+let accEmail = "shivam7557@gmail.com";
+var accPassword = "7668";
+accCity = "Madhya Pradesh";
+let accState;
+
+// accId= 2;
+accEmail = "stk6876@gmail.com";
+accPassword = "68668";
+accCity = "up"
+accState = "Deihi";
+console.table([accId, accEmail, accPassword, accCity, accState]);
