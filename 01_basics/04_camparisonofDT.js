91519203aba0e0482d825682b0fcaf359af219a7
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-09 12:28:02 +0000

    comparisonofDT.js

---
+++ 01_basics/04_camparisonofDT.js
@@ -0,0 +1,22 @@
+console.log("2" > 1)
+console.log("02" > 1)
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log("2" == 2)
+console.log("2" === 2)
+
+// DT Summary
+const id = Symbol('7686')
+const anotherId = Symbol('7686')
+console.log(typeof id)
+console.log(id == anotherId)
+console.log(id === anotherId)
+
+const myfunction = function(){
+    console.log("Hello my is Shivam Kushwaha")
+}
+console.log(typeof myfunction)
