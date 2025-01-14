1ed8b4d40d0fcbd2f80fe3ecace29123b867da41
Shivam Kushwaha <128398378+Shivam0091@users.noreply.github.com>
2025-01-14 15:26:52 +0000

    changes done

--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -57,7 +57,7 @@ const newNums = myNumers
 console.log(newNums);
 
 // --------------------------------------------------------------------
-const myNums = [1, 2, 3]
+// const myNums = [1, 2, 3]
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
